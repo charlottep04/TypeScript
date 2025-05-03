ef5e274ba33a81cc4c58706156dff76df61b336d
Charlotte Poupat <charlotte.poupat@telecom-paris.fr>
2025-05-03 17:58:00 +0200

    VF2

--- 03_modules/03.Poupat/src/scripts/moduleTravail.ts
+++
@@ -1,9 +0,0 @@
-// @filename: moduleTravail.ts
-
-//Pour un salarié travaillant du lundi au vendredi avec 5 semaines de congés payés, 
-// le nombre de jours travaillés est généralement de 227 jours en 2024
-
-export function calculerJoursRestants(n: number) {
-    return 227 - n;
-}
-
--- 03_modules/03.Poupat/src/scripts/moduleVacances.ts
+++
@@ -1,8 +0,0 @@
-// @filename: moduleVacances.ts
-
-//Pour un salarié travaillant du lundi au vendredi avec 5 semaines de congés payés, 
-// le nombre de jours de vacances est de 40 jours environ en 2024
-
-export function calculerJoursRestants(n: number) {
-    return 40 - n;
-}
